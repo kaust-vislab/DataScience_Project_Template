79aadfbb2524f30f6ca557c03951056fbe2a33d4
Didier Barradas Bautista <didier.barradas@gmail.com>
2023-11-12 16:15:59 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use this template to start you project on data science',
+    author='KAUST_KVL_KSL',
+    license='MIT',
+)
